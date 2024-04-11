f2bf6d14921d1319f71e2e04033b169e83b4adba
komandu5 <komandu5@msu.edu>
2024-04-11 17:47:18 -0400

    starter

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Starship Central`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
